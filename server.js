633a2fc4b44bceeeaa838139f022a53479589ad5
skynetfrank <frank.uah@gmail.com>
2021-12-14 15:45:18 -0400

    amazonia22 deploy

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 import http from 'http';
 import { Server } from 'socket.io';
 import express from 'express';
@@ -16,7 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona');
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona').then(() => {
+  console.log('conectado a mongodb');
+});
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
@@ -30,9 +31,7 @@ app.get('/api/config/google', (req, res) => {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
 // app.get('/', (req, res) => {
 //   res.send('Server is ready');
 // });
@@ -47,27 +46,27 @@ const httpServer = http.Server(app);
 const io = new Server(httpServer, { cors: { origin: '*' } });
 const users = [];
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('connection', socket.id);
   socket.on('disconnect', () => {
-    const user = users.find((x) => x.socketId === socket.id);
+    const user = users.find(x => x.socketId === socket.id);
     if (user) {
       user.online = false;
       console.log('Offline', user.name);
-      const admin = users.find((x) => x.isAdmin && x.online);
+      const admin = users.find(x => x.isAdmin && x.online);
       if (admin) {
         io.to(admin.socketId).emit('updateUser', user);
       }
     }
   });
-  socket.on('onLogin', (user) => {
+  socket.on('onLogin', user => {
     const updatedUser = {
       ...user,
       online: true,
       socketId: socket.id,
       messages: [],
     };
-    const existUser = users.find((x) => x._id === updatedUser._id);
+    const existUser = users.find(x => x._id === updatedUser._id);
     if (existUser) {
       existUser.socketId = socket.id;
       existUser.online = true;
@@ -75,7 +74,7 @@ io.on('connection', (socket) => {
       users.push(updatedUser);
     }
     console.log('Online', user.name);
-    const admin = users.find((x) => x.isAdmin && x.online);
+    const admin = users.find(x => x.isAdmin && x.online);
     if (admin) {
       io.to(admin.socketId).emit('updateUser', updatedUser);
     }
@@ -84,26 +83,26 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('onUserSelected', (user) => {
-    const admin = users.find((x) => x.isAdmin && x.online);
+  socket.on('onUserSelected', user => {
+    const admin = users.find(x => x.isAdmin && x.online);
     if (admin) {
-      const existUser = users.find((x) => x._id === user._id);
+      const existUser = users.find(x => x._id === user._id);
       io.to(admin.socketId).emit('selectUser', existUser);
     }
   });
 
-  socket.on('onMessage', (message) => {
+  socket.on('onMessage', message => {
     if (message.isAdmin) {
-      const user = users.find((x) => x._id === message._id && x.online);
+      const user = users.find(x => x._id === message._id && x.online);
       if (user) {
         io.to(user.socketId).emit('message', message);
         user.messages.push(message);
       }
     } else {
-      const admin = users.find((x) => x.isAdmin && x.online);
+      const admin = users.find(x => x.isAdmin && x.online);
       if (admin) {
         io.to(admin.socketId).emit('message', message);
-        const user = users.find((x) => x._id === message._id && x.online);
+        const user = users.find(x => x._id === message._id && x.online);
         user.messages.push(message);
       } else {
         io.to(socket.id).emit('message', {
